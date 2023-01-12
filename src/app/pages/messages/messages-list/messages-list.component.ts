4dbe02d553d200b0c2555e619bfeb28c0fcc8903
Kishan v <kishan@ifourtechnolab.com>
2023-01-12 16:09:18 +0530

    sorting, minor fixes

--- src/app/pages/messages/messages-list/messages-list.component.ts
+++ src/app/pages/messages/messages-list/messages-list.component.ts
@@ -3,7 +3,6 @@ import { MatSort, Sort } from '@angular/material/sort';
 import { Store } from '@ngrx/store';
 import { Subject, takeUntil } from 'rxjs';
 import { MessageData } from 'src/app/shared/models/message';
-import { MessageServiceService } from 'src/app/shared/services/message-service.service';
 import * as fromRoot from 'src/app/store';
 import { getMessages } from 'src/app/store/actions/app.actions';
 @Component({
@@ -13,14 +12,13 @@ import { getMessages } from 'src/app/store/actions/app.actions';
 })
 export class MessageListComponent {
   dataSource: any = [];
-  displayedColumns: string[] = ['id', 'name', 'message', 'datatime'];
+  displayedColumns: string[] = ['id', 'name', 'message', 'datetime'];
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   isLoading: boolean = true;
   destroy$: Subject<boolean> = new Subject<boolean>();
   messageData: MessageData;
 
   constructor(
-    private messageService: MessageServiceService,
     private store: Store<MessageData>
   ) {
     this.store
@@ -30,10 +28,24 @@ export class MessageListComponent {
         if (data.IsGetLoadingSuccess && data.MessageData) {
           this.dataSource = Object.values(data.MessageData);
           this.dataSource.pop();
+          this.sortByDate(this.dataSource);
+          this.isLoading = false;
         }
-        this.isLoading = false;
       });
     this.loadMessageList();
+  }
+  sortByDate(data) {
+    this.dataSource = data.sort((a, b) => {
+      var DateA = new Date(a.datetime);
+      var DateB = new Date(b.datetime);
+      if (DateA > DateB) {
+        return -1;
+      } else if (DateA < DateB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
   }
 
   loadMessageList() {
@@ -57,11 +69,19 @@ export class MessageListComponent {
           return this.compare(a.name, b.name, isAsc);
         case 'message':
           return this.compare(a.message, b.message, isAsc);
+        case 'datatime':
+          return this.compareDate(a.datatime, b.datatime, isAsc);
         default:
           return 0;
       }
     });
     this.isLoading = false;
+  }
+  compareDate(a: number | string, b: number | string, isAsc: boolean) {
+    return (
+      (new Date(a).getTime() < new Date(b).getTime() ? -1 : 1) *
+      (isAsc ? 1 : -1)
+    );
   }
   compare(a: number | string, b: number | string, isAsc: boolean) {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
